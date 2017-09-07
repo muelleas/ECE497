74a0670fc226ea06f40f693671821d6fb6dac1de
muelleas <muelleas@rose-hulman.edu>
2017-09-06 21:39:39 -0400

    documented fourbuttons and made testing file

--- hw02/fourbuttons.js
+++ hw02/fourbuttons.js
@@ -1,4 +1,13 @@
 #!/usr/bin/env node
+/* File: fourbuttons.js
+ * Author: Andrew Mueller
+ *
+ * This file takes fur buttons and toggles four LEDs, One for each button.
+ * The buttons are plugged in to GP0 while the LEDs are plugged in through GP! 
+ * making use of  220 ohm resistors.
+*/
+
+//Setting up required variables
 var b = require('bonescript');
 
 var led = ["GP1_3", "GP1_4", "RED", "GREEN"];
@@ -17,29 +26,30 @@ b.pinMode(button[1], b.INPUT);
 b.pinMode(button[2], b.INPUT);
 b.pinMode(button[3], b.INPUT);
 
-b.attachInterrupt(button[0], toggleB0, b.CHANGE);
+//Attaching interrups
+b.attachInterrupt(button[0], toggleB0, b.CHANGE);  //Could potetally be changesto falling edge but that would mess with my debounce method
 b.attachInterrupt(button[1], toggleB1, b.CHANGE);
 b.attachInterrupt(button[2], toggleB2, b.CHANGE);
 b.attachInterrupt(button[3], toggleB3, b.CHANGE);
 
+//Init the leds (on)
 b.digitalWrite(led[0], 0);
 b.digitalWrite(led[1], 0);
 b.digitalWrite(led[2], 0);
 b.digitalWrite(led[3], 0);
 
+//The interups to be used by the buttons
 function toggleB0(x) {
-  console.log("0");
-  setTimeout(function(){
-    if (x.value == 0 && prev[0] == 1) {
-      (state[0] == 0) ? state[0] = 1 : state[0] = 0;
-      b.digitalWrite(led[0], state[0]);
+  setTimeout(function(){ // debounces the button
+    if (x.value == 0 && prev[0] == 1) { //checks for falling edge
+      (state[0] == 0) ? state[0] = 1 : state[0] = 0;  //toggles the state
+      b.digitalWrite(led[0], state[0]);  //changes the light
     }
-  prev[0] = x.value;
+  prev[0] = x.value;  // updates the previous value
   }, 25); 
 }
 
 function toggleB1(x) {
-  console.log("1");
   setTimeout(function(){
     if (x.value == 0 && prev[1] == 1) {
       (state[1] == 0) ? state[1] = 1 : state[1] = 0;
@@ -50,7 +60,6 @@ function toggleB1(x) {
 }
 
 function toggleB2(x) {
-  console.log("2");
   setTimeout(function(){
     if (x.value == 0 && prev[2] == 1) {
       (state[2] == 0) ? state[2] = 1 : state[2] = 0;
@@ -61,7 +70,6 @@ function toggleB2(x) {
 }
 
 function toggleB3(x) {
-  console.log("3");
   setTimeout(function(){
     if (x.value == 0 && prev[3] == 1) {
       (state[3] == 0) ? state[3] = 1 : state[3] = 0;
