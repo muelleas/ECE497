a11eb7f55daad7e6062643005f29cc7b5e051d5e
Andrew Mueller <muelleas@rose-hulman.edu>
2017-09-08 16:16:39 +0000

    simplified interruppssdf

--- hw02/fourbuttons.js
+++ hw02/fourbuttons.js
@@ -14,6 +14,7 @@ var led = ["GP1_3", "GP1_4", "RED", "GREEN"];
 var state = [0, 0, 0 ,0];
 var prev = [1, 1, 1, 1];
 var button = ["GP0_3", "GP0_4", "GP0_5", "GP0_6"];
+var remap = ["GPIO1_25", "GPIO1_17", "GPIO3_20", "SPI1_CS0"];  // x.pin.name returns these names for the pins so this array lets me convert between the names used
 
 //set pinMode
 b.pinMode(led[0], b.OUTPUT);
@@ -27,10 +28,10 @@ b.pinMode(button[2], b.INPUT);
 b.pinMode(button[3], b.INPUT);
 
 //Attaching interrups
-b.attachInterrupt(button[0], toggleB0, b.CHANGE);  //Could potetally be changesto falling edge but that would mess with my debounce method
-b.attachInterrupt(button[1], toggleB1, b.CHANGE);
-b.attachInterrupt(button[2], toggleB2, b.CHANGE);
-b.attachInterrupt(button[3], toggleB3, b.CHANGE);
+b.attachInterrupt(button[0], toggle, b.CHANGE);  //Could potetally be changesto falling edge but that would mess with my debounce method
+b.attachInterrupt(button[1], toggle, b.CHANGE);
+b.attachInterrupt(button[2], toggle, b.CHANGE);
+b.attachInterrupt(button[3], toggle, b.CHANGE);
 
 //Init the leds (on)
 b.digitalWrite(led[0], 0);
@@ -38,43 +39,15 @@ b.digitalWrite(led[1], 0);
 b.digitalWrite(led[2], 0);
 b.digitalWrite(led[3], 0);
 
-//The interups to be used by the buttons
-function toggleB0(x) {
+//The interup to be used by the buttons
+function toggle(x) {
+  console.log(x.pin.name);
+  var num = remap.indexOf(x.pin.name);
   setTimeout(function(){ // debounces the button
-    if (x.value == 0 && prev[0] == 1) { //checks for falling edge
-      (state[0] == 0) ? state[0] = 1 : state[0] = 0;  //toggles the state
-      b.digitalWrite(led[0], state[0]);  //changes the light
+    if (x.value == 0 && prev[num] == 1) { //checks for falling edge
+      (state[num] == 0) ? state[num] = 1 : state[num] = 0;  //toggles the state
+      b.digitalWrite(led[num], state[num]);  //changes the light
     }
-  prev[0] = x.value;  // updates the previous value
-  }, 25); 
-}
-
-function toggleB1(x) {
-  setTimeout(function(){
-    if (x.value == 0 && prev[1] == 1) {
-      (state[1] == 0) ? state[1] = 1 : state[1] = 0;
-      b.digitalWrite(led[1], state[1]);
-    }
-  prev[1] = x.value;
-  }, 25); 
-}
-
-function toggleB2(x) {
-  setTimeout(function(){
-    if (x.value == 0 && prev[2] == 1) {
-      (state[2] == 0) ? state[2] = 1 : state[2] = 0;
-      b.digitalWrite(led[2], state[2]);
-    }
-  prev[2] = x.value;
-  }, 25); 
-}
-
-function toggleB3(x) {
-  setTimeout(function(){
-    if (x.value == 0 && prev[3] == 1) {
-      (state[3] == 0) ? state[3] = 1 : state[3] = 0;
-      b.digitalWrite(led[3], state[3]);
-    }
-  prev[3]= x.value;
+  prev[num] = x.value;  // updates the previous value
   }, 25); 
 }
