e2c5628554dccf5ea2a8d722a562b2002c442bc7
muelleas <muelleas@rose-hulman.edu>
2017-09-11 12:54:14 -0400

    did docs on python files

--- hw02/EtchWButtons.py
+++ hw02/EtchWButtons.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python3
+# File: EthchWButtons.py
+# Author: Andrew Mueller
+#
+# This file takes input form 4 buttons on GP0 and the PAU button. With these inputs a game of Etch-a-sketch is played in the terminal.
+#The buttons on GP0 act as directionals and PAU is the reset.
 
 import Adafruit_BBIO.GPIO as GPIO
 import time
 
-button = ["GP0_3", "GP0_4", "GP0_5", "GP0_6"]
-map = {button[0]:'r', button[1]:'d', button[2]:'l', button[3]:'u'}
-resetButton = "PAUSE"
+button = ["GP0_3", "GP0_4", "GP0_5", "GP0_6"]   # the buttons used
+map = {button[0]:'r', button[1]:'d', button[2]:'l', button[3]:'u'} #map the buttons to their dirrection
+resetButton = "PAUSE"  #the clear button
 
+#setup GPIO
 GPIO.setup(button[0], GPIO.IN)
 GPIO.setup(button[1], GPIO.IN)
 GPIO.setup(button[2], GPIO.IN)
 GPIO.setup(button[3], GPIO.IN)
 GPIO.setup(resetButton, GPIO.IN)
 
+#Game board setup
 size = 8
 xPos = 1
 yPos = 1
@@ -26,25 +33,24 @@ array = [[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
          [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
          [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']];
 
-def reset(channel = 'NULL'):
-    for i in range(0, size+1):
+def reset(channel = 'NULL'):  #clears the board
+    for i in range(0, size+1):  #set all tiles to empty
         for j in range(0, size+1):
             array[i][j] = ' '
-    for i in range(0, size):
+    for i in range(0, size):     #place the side numbers
         array[0][i+1] = i
         array[i+1][0] = i
-    array[xPos][yPos] = 'o'
+    array[xPos][yPos] = 'o'     #place curser
     printArray();
 
-def printArray():
+def printArray():		#prints the board
     for i in range(0, size+1):
         print(*array[i])
 
 def updateBoard(channel):
-    print("channel"+ channel)
     global xPos
     global yPos
-    if map[channel] == 'd':
+    if map[channel] == 'd':   #moves the currsor
         if xPos < size:
             xPos += 1
     if map[channel] == 'u':
@@ -56,11 +62,11 @@ def updateBoard(channel):
     if map[channel] == 'l':
         if yPos > 1:
             yPos += -1
-    array[xPos][yPos] = 'o'
+    array[xPos][yPos] = 'o'   #place cursor
     printArray()
-    time.sleep(.05)
+    time.sleep(.05)	#debounce button
 
-GPIO.add_event_detect(button[0], GPIO.FALLING, callback=updateBoard)
+GPIO.add_event_detect(button[0], GPIO.FALLING, callback=updateBoard)	#adding the interrupts
 GPIO.add_event_detect(button[1], GPIO.FALLING, callback=updateBoard)
 GPIO.add_event_detect(button[2], GPIO.FALLING, callback=updateBoard)
 GPIO.add_event_detect(button[3], GPIO.FALLING, callback=updateBoard)
--- hw02/fourbuttons.py
+++ hw02/fourbuttons.py
@@ -1,13 +1,15 @@
-#!/usr/bin/env python3
-# Reads the PAUSE button using interupts and sets the LED
-# Pin table at https://github.com/beagleboard/beaglebone-blue/blob/master/BeagleBone_Blue_Pin_Table.csv
+#!/usr/bin/env python30
+# File: fourbuttons.py
+# Author: Andrew Mueller
+#
+# This file takes input form 4 buttons on GP0 and toggles 4 leds on GP1
 
 # Import PyBBIO library:
 import Adafruit_BBIO.GPIO as GPIO
 import time
 
-button = ["GP0_3", "GP0_4", "GP0_5", "GP0_6"]
-led = ["GP1_3", "GP1_4", "RED", "GREEN"]
+button = ["GP0_3", "GP0_4", "GP0_5", "GP0_6"]  # the buttons used
+led = ["GP1_3", "GP1_4", "RED", "GREEN"] # LEDs used
 
 # Set the GPIO pins:
 GPIO.setup(led[0],    GPIO.OUT)
@@ -20,7 +22,7 @@ GPIO.setup(button[1], GPIO.IN)
 GPIO.setup(button[2], GPIO.IN)
 GPIO.setup(button[3], GPIO.IN)
 
-# Turn on both LEDs
+# Turn on the LEDs
 GPIO.output(led[0], 1)
 GPIO.output(led[1], 1)
 GPIO.output(led[2], 1)
@@ -29,15 +31,13 @@ GPIO.output(led[3], 1)
 # Map buttons to LEDs
 map = {button[0]: led[0], button[1]: led[1], button[2]: led[2], button[3]: led[3]}
 
-def updateLED(channel):
-    print("channel = " + channel)
-    state = GPIO.input(channel)
-    GPIO.output(map[channel], state)
-    print(map[channel] + " Toggled")
+def updateLED(channel):      #interrupt
+    state = GPIO.input(channel)     #get the state
+    GPIO.output(map[channel], state) #toggle the LED
 
 print("Running...")
 
-GPIO.add_event_detect(button[0], GPIO.BOTH, callback=updateLED) # RISING, FALLING or BOTH
+GPIO.add_event_detect(button[0], GPIO.BOTH, callback=updateLED) #adding the interrupt
 GPIO.add_event_detect(button[1], GPIO.BOTH, callback=updateLED)
 GPIO.add_event_detect(button[2], GPIO.BOTH, callback=updateLED)
 GPIO.add_event_detect(button[3], GPIO.BOTH, callback=updateLED)
