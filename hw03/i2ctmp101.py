aa2ec5f37483b6c847f1220055cbd5d771eb1d92
Andrew Mueller <muelleas@rose-hulman.edu>
2017-09-13 15:09:13 +0000

    2 tmps woring with allerts just needs docs

--- hw03/i2ctmp101.py
+++ hw03/i2ctmp101.py
@@ -5,27 +5,35 @@ import Adafruit_BBIO.GPIO as GPIO
 import smbus
 import time
 bus = smbus.SMBus(1)
-address = 0x48
-alert = "GP0_3"
+address0 = 0x48
+address1 = 0x49
+alert0 = "GP0_3"
+alert1 = "GP0_4"
+map = {alert0: address0, alert1: address1}
 
-GPIO.setup(alert, GPIO.IN)
+GPIO.setup(alert0, GPIO.IN)
+GPIO.setup(alert1, GPIO.IN)
 
-bus.write_byte_data(address, 3, 23)
-bus.write_byte_data(address, 2, 22)
+bus.write_byte_data(address0, 3, 24)
+bus.write_byte_data(address0, 2, 24)
+
+bus.write_byte_data(address1, 3, 24)
+bus.write_byte_data(address1, 2, 24)
+
 
 def tempPrint(channel):
-    temp = bus.read_byte_data(address, 0)
+    device = map[channel]
+    temp = bus.read_byte_data(device, 0)
     print("hot")
-    print (temp)
+    print (temp*9/5+32)
 
-GPIO.add_event_detect(alert, GPIO.BOTH, callback=tempPrint)
+GPIO.add_event_detect(alert0, GPIO.FALLING, callback=tempPrint)
+GPIO.add_event_detect(alert1, GPIO.FALLING, callback=tempPrint)
 
 try:
     print("running")
     while True:
-        #temp = bus.read_byte_data(address, 0)
-        #print(temp)
-        time.sleep(.25)
+        time.sleep(100)
 except KeyboardInterrupt:
     print("cleanup")
     GPIO.cleanup()
