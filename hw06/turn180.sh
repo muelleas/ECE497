f802c4d8a726feac5279f14368a5deee87bd26f4
Andrew Mueller <muelleas@rose-hulman.edu>
2017-10-06 15:42:28 +0000

    added files to turn screen

---
+++ hw06/turn180.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#turn the display to 90 degrees
+# Connect the display before running this.
+
+sudo bash << EOF
+    # remove the framebuffer modules
+    rmmod --force fb_ili9341
+    rmmod --force fbtft
+    rmmod --force fbtft_device
+    
+    sleep 1
+    
+    # Insert the framebuffer modules
+    modprobe fbtft_device name=adafruit28 busnum=1 rotate=180 gpios=reset:113,dc:116 cs=0
+
+EOF
