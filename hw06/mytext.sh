50cd5e28445fea9f0d1eed5555951f8e3ff6c113
Andrew Mueller <muelleas@rose-hulman.edu>
2017-10-06 17:19:42 +0000

    small change to mytext

--- hw06/mytext.sh
+++ hw06/mytext.sh
@@ -9,7 +9,7 @@ TMP_FILE=/tmp/frame.png
 #place boris on the frame with some text on it.
 convert boris.png -resize $SIZE -fill black -font Times-Roman -pointsize 35\
        	-draw "text 0,50 'By Andrew'" -draw "text 0,225 'Boris.png'"\
-       	-append $TMP_FILE
+       	$TMP_FILE
 
 #show frame
 sudo fbi -noverbose -T 1 $TMP_FILE
